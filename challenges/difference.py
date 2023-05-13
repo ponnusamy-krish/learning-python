3f6c6136b457e9a737fcce04f596da1d1bd0a41f
sam <ponnusamykrish23@gmail.com>
2023-05-13 07:58:51 +0530

    set

---
+++ challenges/difference.py
@@ -0,0 +1,6 @@
+eng = int(input())
+set1 = set(map(int,input().split()))
+fre = int(input())
+set2 = set(map(int,input().split()))
+print(set1.difference(set2))
+
---
+++ learning/set.py
@@ -0,0 +1,3 @@
+a_set = {1,2,3,4,5,6,7}
+a_set.discard(5)
+print(a_set)
